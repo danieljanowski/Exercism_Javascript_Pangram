4e8410d1c16e27ce4805d88a51de3cafa06ca60c
danieljanowski <daniel.janowski@gmail.com>
2021-02-06 19:40:06 +0000

    removed unncessary brackets

--- pangram.js
+++ pangram.js
@@ -2,8 +2,7 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 export const isPangram = (sentence) => {
   let allLetters = [...alphabet];
-  for (const letter of sentence) {
+  for (const letter of sentence)
     allLetters = allLetters.filter((l) => l !== letter.toLowerCase());
-  }
   return allLetters.length === 0 ? true : false;
 };
