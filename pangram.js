55acfe0c5676b22a557dc6a8e8d962cec85fd884
danieljanowski <daniel.janowski@gmail.com>
2021-02-06 19:25:24 +0000

    refactored

--- pangram.js
+++ pangram.js
@@ -2,10 +2,8 @@ const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
 export const isPangram = (sentence) => {
   let allLetters = [...alphabet];
-  for (let letter of sentence) {
-    allLetters = allLetters.filter(
-      (currentLetter) => letter.toLowerCase() !== currentLetter
-    );
+  for (const letter of sentence) {
+    allLetters = allLetters.filter((l) => l !== letter.toLowerCase());
   }
   return allLetters == 0 ? true : false;
 };
