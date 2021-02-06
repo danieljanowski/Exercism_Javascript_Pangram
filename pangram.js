b5c643cb287ff8548b9f4afdeb9eaac28065fac2
danieljanowski <daniel.janowski@gmail.com>
2021-02-06 19:14:16 +0000

    passing all tests

--- pangram.js
+++ pangram.js
@@ -1,8 +1,11 @@
-//
-// This is only a SKELETON file for the 'Pangram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-export const isPangram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isPangram = (sentence) => {
+  let allLetters = [...alphabet];
+  for (let letter of sentence) {
+    allLetters = allLetters.filter(
+      (currentLetter) => letter.toLowerCase() !== currentLetter
+    );
+  }
+  return allLetters == 0 ? true : false;
 };
