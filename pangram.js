f30774d9eb92b046b73d34c9d7006f5a2f94735d
danieljanowski <daniel.janowski@gmail.com>
2021-03-13 10:51:29 +0000

    refactoring - removed ternary

--- pangram.js
+++ pangram.js
@@ -4,5 +4,5 @@ export const isPangram = (sentence) => {
   let allLetters = [...alphabet];
   for (const letter of sentence)
     allLetters = allLetters.filter((l) => l !== letter.toLowerCase());
-  return allLetters.length === 0 ? true : false;
+  return allLetters.length === 0;
 };
